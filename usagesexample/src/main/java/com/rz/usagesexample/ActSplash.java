9fbb9b8b824598589544f6165df7bd3dd0b4817d
Rz Rasel <Rz Rasel>
2017-08-26 23:34:21 +0600

    Add Project

--- usagesexample/src/main/java/com/rz/usagesexample/ActSplash.java
+++ usagesexample/src/main/java/com/rz/usagesexample/ActSplash.java
@@ -3,11 +3,16 @@ package com.rz.usagesexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.rz.sharpwebview.SharpWebView;
+
 public class ActSplash extends AppCompatActivity {
+    private SharpWebView sysSharpWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_splash);
+        sysSharpWebView = (SharpWebView) findViewById(R.id.sysSharpWebView);
+        sysSharpWebView.onLoadURL("http://jagoron24.com/");
     }
 }
